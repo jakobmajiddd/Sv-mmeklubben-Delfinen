a060fe667eaae8d74b2ba2a6ce779b142c5fb516
martinlaursen97 <martin_laursen21@hotmail.com>
2021-05-11 11:29:25 +0200

    Added members

--- Delfinen/src/member/Member.java
+++ Delfinen/src/member/Member.java
@@ -1,4 +1,52 @@
 package member;
 
-public class Member {
+// @author Martin
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public abstract class Member {
+    private final String FILE_ID;
+    private final String NAME;
+    private final int AGE;
+    private final String EMAIL;
+    private String lastPaymentDate;
+
+    Member(String id, String name, int age, String email, Date date) {
+        this.FILE_ID = id;
+        this.NAME = name;
+        this.AGE = age;
+        this.EMAIL = email;
+        this.lastPaymentDate = convertDateToString(date);
+    }
+
+    public abstract String toFileFormat();
+
+    public String convertDateToString(Date date) {
+        DateFormat df = new SimpleDateFormat("d:MM:y");
+        Date today = Calendar.getInstance().getTime();
+        return df.format(today);
+    }
+
+    public String getFILE_ID() {
+        return FILE_ID;
+    }
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public int getAGE() {
+        return AGE;
+    }
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+
+    public String getLastPaymentDate() {
+        return lastPaymentDate;
+    }
 }
--- Delfinen/src/member/PassiveMember.java
+++ Delfinen/src/member/PassiveMember.java
@@ -1,4 +1,18 @@
 package member;
 
-public class PassiveMember {
+import java.util.Date;
+
+public class PassiveMember extends Member {
+    PassiveMember(String name, int age, String email, Date date) {
+        super("pm", name, age, email, date);
+    }
+
+    @Override
+    public String toFileFormat() {
+        return getFILE_ID()
+                + "_" + getNAME()
+                + "_" + getAGE()
+                + "_" + getEMAIL()
+                + "_" + getLastPaymentDate();
+    }
 }
