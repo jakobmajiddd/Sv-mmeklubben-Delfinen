2c5f11454f37844dd2343f18537a3a3910b5c0ad
Silke Marie <silk2918@stud.kea.dk>
2021-05-12 10:22:44 +0200

    accountant switch opdatering

--- Delfinen/src/staff/Accountant.java
+++ Delfinen/src/staff/Accountant.java
@@ -2,6 +2,7 @@ package staff;
 
 
 import main.Controller;
+import main.UI;
 
 // @author Silke
 public class Accountant {
@@ -9,7 +10,6 @@ public class Accountant {
   private int juniorSubscription = 1000;
   private double seniorSubscription = 1600;
   private double over60DiscountPercentage = 0.25;
-  private double over60Subscription;
 
 
   public int getPassiveSubscription() {
@@ -24,26 +24,47 @@ public class Accountant {
     return seniorSubscription;
   }
 
-  public double getOver60Subscription() {
-    return over60Subscription;
+  public double getOver60DiscountPercentage() {
+    return over60DiscountPercentage;
   }
 
+  UI ui = new UI();
+  Finance finance = new Finance();
 
-  Controller controller = new Controller();
+  public void accountantMenuDisplay() {
+    boolean isRunning;
 
+    do {
+      isRunning = true;
 
-  public double over60Discount() { //double seniorSubscription, double over60DiscountPercentage
-    double discount = seniorSubscription * over60DiscountPercentage;
-    over60Subscription = seniorSubscription - discount;
-    System.out.println(over60Subscription); //test til at se om det regner rigtigt
-    return over60Subscription;
-  }
+      ui.printFinanceMenu();
 
- /* public void arrears() { //skal tage ArrayListen som parameter
-    for (int i = 0; i < controller.getMembers().size(); i++) {
-      if (//lastPaymentDate == moreThanOneYearAgo) {
-        //flyt til betalingsstoppet-ArrayListe?
-        //+ send betalingspåmindelse ? fra fil?
+      switch (ui.getInput()) {
+
+        case 1:
+          System.out.println("Yearly expected total revenue");
+          finance.expectedRevenue();
+          break;
+        case 2:
+          System.out.println("Expected revenue for passive members");
+          finance.yearlyPassiveRevenue();
+          break;
+        case 3:
+          System.out.println("Expected revenue for junior members");
+          finance.yearlyJuniorRevenue();
+        case 4:
+          System.out.println("Expected revenue for senior members");
+          finance.yearlySeniorRevenue();
+        case 5:
+          System.out.println("Expected revenue for senior discounted members");
+          finance.yearlySeniorDiscountRevenue();
+        case 6:
+          System.out.println("Show members in arrears");
+          //TO DO
+        case 9:
+          System.out.println("Return to main menu");
+          isRunning = false;
       }
-    }*/
+    } while (isRunning);
   }
+}
--- Delfinen/src/staff/Finance.java
+++ Delfinen/src/staff/Finance.java
@@ -17,24 +17,37 @@ public class Finance {
   private int juniorRevenue;
   private double seniorRevenue;
   private double seniorDiscountRevenue;
+  private double over60Subscription;
 
   Accountant accountant = new Accountant();
   Controller controller = new Controller();
 
 
-  public double expectedRevenue() {
+  public double over60Discount() {
+    double discount = accountant.getSeniorSubscription() * accountant.getOver60DiscountPercentage();
+    over60Subscription = accountant.getSeniorSubscription() - discount;
+    System.out.println(over60Subscription);
+    return over60Subscription;
+  }
 
+
+  public double expectedRevenue() {
+    over60Discount();
 
     ArrayList<Member> members = controller.getMembers();
     members.add(new PassiveMember("Jens", 29, "jenner@gmail.com", new Date()));
+    members.add(new PassiveMember("Jakob", 21, "j@gmail.com", new Date()));
+
+
     for (Member m : members) {
       if (m instanceof PassiveMember) {
         passiveRevenue += accountant.getPassiveSubscription();
+        System.out.println(passiveRevenue);
       } else if (m instanceof FitnessMember) {
         if (m.getAGE() < 18) {
           juniorRevenue += accountant.getJuniorSubscription();
         } else if (m.getAGE() < 60) {
-          seniorDiscountRevenue += accountant.getOver60Subscription();
+          seniorDiscountRevenue += over60Subscription;
         } else {
           seniorRevenue += accountant.getSeniorSubscription();
         }
@@ -43,13 +56,42 @@ public class Finance {
         if (m.getAGE() < 18) {
           juniorRevenue += accountant.getJuniorSubscription();
         } else if (m.getAGE() < 60) {
-          seniorDiscountRevenue += accountant.getOver60Subscription();
+          seniorDiscountRevenue += over60Subscription;
         } else {
           seniorRevenue += accountant.getSeniorSubscription();
         }
       }
     }
     yearlyRevenue = passiveRevenue + juniorRevenue + seniorRevenue + seniorDiscountRevenue;
+    System.out.println(yearlyRevenue);
     return yearlyRevenue;
   }
+
+  public void yearlyPassiveRevenue() {
+    expectedRevenue();
+    System.out.println(passiveRevenue);
+  }
+
+  public void yearlyJuniorRevenue() {
+    expectedRevenue();
+    System.out.println(juniorRevenue);
+  }
+
+  public void yearlySeniorRevenue() {
+    expectedRevenue();
+    System.out.println(seniorRevenue);
+  }
+
+  public void yearlySeniorDiscountRevenue() {
+    expectedRevenue();
+    System.out.println(seniorDiscountRevenue);
+  }
+
+ /* public void arrears() { //skal tage ArrayListen som parameter
+    for (int i = 0; i < controller.getMembers().size(); i++) {
+      if (//lastPaymentDate == moreThanOneYearAgo) {
+        //flyt til betalingsstoppet-ArrayListe?
+        //+ send betalingspåmindelse ? fra fil?
+      }
+    }*/
 }
